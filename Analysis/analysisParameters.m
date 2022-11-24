c075bea9e291796d9acc495dde01bb566958512a
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-11-24 22:46:27 +0100

    Create analysisParameters.m

---
+++ Analysis/analysisParameters.m
@@ -0,0 +1,192 @@
+function P = analysisParameters()
+% Here is located all the common variables, paths, and parameters that get
+% repeatedly called by more than one preprocessing script.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Labels
+
+P.Participants = {'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07', 'P08', ...
+    'P09', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15', 'P16', 'P17', 'P19'};
+
+P.AllTasks = {'Match2Sample', 'LAT', 'PVT', 'SpFT', 'Game', 'Music'};
+P.TaskLabels = {'STM', 'LAT', 'PVT', 'Speech', 'Game', 'Music'};
+
+
+P.Nights = {'Baseline', 'NightPre', 'NightPost'};
+
+Labels.Sessions.IDs = {'BaselineBeam', 'MainPre', 'Session1Beam', 'Session2Beam1', 'Session2Beam2', 'Session2Beam3', 'MainPost'};
+Labels.Sessions.Labels = {'BL', 'Pre', 'SD1', 'SD2.1', 'SD2.2', 'SD2.3', 'Post'};
+
+Labels.Sessions_SD.IDs = { 'Session2Beam1', 'Session2Beam2', 'Session2Beam3'};
+Labels.Sessions_BL.IDs = {'BaselineBeam',  'MainPre', 'MainPost'};
+
+Labels.Tally = {'Lapses', 'Late', 'Correct'};
+
+Labels.Task = 'LAT';
+Labels.logBands = [1 2 4 8 16 32]; % x markers for plot on log scale
+Labels.Bands = [1 4 8 15 25 35 40]; % normal scale
+Labels.FreqLimits = [1 40];
+Labels.zPower = 'PSD z-scored';
+Labels.Power = 'PSD Amplitude (\muV^2/Hz)';
+Labels.Frequency = 'Frequency (Hz)';
+Labels.Epochs = {'Encoding', 'Retention1', 'Retention2', 'Probe'}; % for M2S task
+Labels.Amplitude = 'Amplitude (\muV)';
+Labels.Time = 'Time (s)';
+Labels.ES = "Hedge's G";
+Labels.t = 't-values';
+Labels.Correct = '% Correct';
+Labels.RT = 'RT (s)';
+P.Labels = Labels;
+
+Triggers.SyncEyes = 'S192';
+Triggers.Start = 'S  1';
+Triggers.End = 'S  2';
+
+P.Triggers = Triggers;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Locations
+
+% if eeglab has not run, run it so all the subdirectories get added
+if ~exist('topoplot', 'file')
+    eeglab
+    close all
+end
+
+% same for plotting scripts, saved to a different repo (https://github.com/snipeso/chart)
+if ~exist('addchARTpaths.m', 'file')
+    addchARTpaths() % TODO, find in folder automatically
+end
+
+
+if exist( 'D:\Data\Raw', 'dir')
+    Core = 'D:\Data\';
+elseif exist( 'F:\Data\Raw', 'dir')
+    Core = 'F:\Data\';
+elseif  exist( 'E:\Data\Raw', 'dir')
+    Core = 'E:\Data\';
+else
+    error('no data disk!')
+end
+
+Paths.Preprocessed = fullfile(Core, 'Preprocessed');
+Paths.Core = Core;
+
+Paths.Datasets = 'G:\LSM\Data\Raw';
+Paths.Data  = fullfile(Core, 'Final'); % where data gets saved once its been turned into something else
+Paths.PaperResults = fullfile(Core, 'Results', 'Theta-SD-vs-WM'); % where figures and tables end up
+% Paths.Paper = 'C:\Users\colas\Dropbox\Research\Publications and Presentations\Sleep\Paper2\Figures';
+% Paths.Poster = 'C:\Users\colas\Dropbox\Research\Publications and Presentations\Sleep\SSSSC2022\Figures';
+% Paths.Powerpoint = 'C:\Users\colas\Dropbox\Research\Projects\HuberSleepLab\LSM\Repeat Figures\MatlabFigures';
+% Paths.PaperStats =  'C:\Users\colas\Dropbox\Research\Publications and Presentations\Sleep\Paper2\Stats';
+Paths.Scoring = fullfile(Core, 'Scoring');
+Paths.Results = fullfile(Core, 'Results\Theta_Bursts');
+
+
+% get path where these scripts were saved
+CD = mfilename('fullpath');
+% Paths.Analysis = fullfile(extractBefore(Paths.Analysis, 'Analysis'));
+Paths.Analysis = fullfile(extractBefore(CD, 'Lapse-Causes'), 'Lapse-Causes');
+
+% add location of subfunctions (from older scripts TODO: copy scripts that
+% I use here
+addpath(fullfile(Paths.Analysis, 'functions','general'))
+addpath(fullfile(Paths.Analysis, 'functions','eeg'))
+addpath(fullfile(Paths.Analysis, 'functions','plots'))
+addpath(fullfile(Paths.Analysis, 'functions','tasks'))
+addpath(fullfile(Paths.Analysis, 'functions','stats'))
+addpath(fullfile(Paths.Analysis, 'functions','questionnaires'))
+run(fullfile(Paths.Analysis, 'functions', 'external', 'addExternalFunctions'))
+
+
+% get path where these scripts were saved
+Paths.Analysis = mfilename('fullpath');
+Paths.Analysis = fullfile(extractBefore(Paths.Analysis, '\Analysis\'));
+
+% add location of subfunctions
+addpath(fullfile(Paths.Analysis, 'functions','general'))
+addpath(fullfile(Paths.Analysis, 'functions','eeg'))
+addpath(fullfile(Paths.Analysis, 'functions','plots'))
+addpath(fullfile(Paths.Analysis, 'functions','tasks'))
+addpath(fullfile(Paths.Analysis, 'functions','stats'))
+addpath(fullfile(Paths.Analysis, 'functions','pupils'))
+addpath(fullfile(Paths.Analysis, 'functions','external'))
+
+P.Paths = Paths;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Plotting settings
+% These use chART (https://github.com/snipeso/chART) plots. Each figure
+% takes a struct that holds all the parameters for plotting (e.g. font
+% names, sizes, etc). These are premade in chART, but can be customized.
+
+
+% plot sizes depending on which screen being used
+Pix = get(0,'screensize');
+if Pix(3) < 2000
+    Format = getProperties({'LSM', 'SmallScreen'});
+else
+    Format = getProperties({'LSM', 'LargeScreen'});
+end
+
+Manuscript = getProperties({'LSM', 'Manuscript'});
+Powerpoint =  getProperties({'LSM', 'Powerpoint'});
+Poster =  getProperties({'LSM', 'Poster'});
+
+P.Manuscript = Manuscript; % for papers
+P.Powerpoint = Powerpoint; % for presentations
+P.Poster = Poster;
+P.Format = Format; % plots just to view data
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Power/EEG information
+
+Bands.Delta = [1 4];
+Bands.Theta = [4 8];
+Bands.Alpha = [8 12];
+Bands.Beta = [15 25];
+Bands.Gamma = [25 35];
+
+%%% Channels and Regions of Interest (ROI)
+Channels = struct();
+
+Channels.Remove = [17, 48, 119]; % channels to remove before FFT
+
+% ROIs selected independently of data
+Frontspot = [22 15 9 23 18 16 10 3 24 19 11 4 124 20 12 5 118 13 6 112];
+Backspot = [66 71 76 84 65 70 75 83 90 69 74 82 89];
+Centerspot = [129 7 106 80 55 31 30 37 54 79 87 105 36 42 53 61 62 78 86 93 104 35 41 47  52 92 98 103 110, 60 85 51 97];
+
+Channels.preROI.Front = Frontspot;
+Channels.preROI.Center = Centerspot;
+Channels.preROI.Back = Backspot;
+
+Format.Colors.preROI = getColors(numel(fieldnames(Channels.preROI)));
+
+
+P.Channels = Channels;
+P.Bands = Bands;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Stats parameters
+
+StatsP = struct();
+
+StatsP.ANOVA.ES = 'eta2';
+StatsP.ANOVA.ES_lims = [0 1];
+StatsP.ANOVA.nBoot = 2000;
+StatsP.ANOVA.pValue = 'pValueGG';
+StatsP.ttest.nBoot = 2000;
+StatsP.ttest.dep = 'pdep'; % use 'dep' for ERPs, pdep for power
+StatsP.Alpha = .05;
+StatsP.Trend = .1;
+StatsP.Paired.ES = 'hedgesg';
+StatsP.Paired.Benchmarks = -2:.5:2;
+StatsP.FreqBin = 1; % # of frequencies to bool in spectrums stats
+StatsP.minProminence = .1; % minimum prominence for when finding clusters of g values
+P.StatsP = StatsP;
+
+
