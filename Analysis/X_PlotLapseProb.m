3b834736d165b46e666c0cc01ea8d679a7b3dded
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-12-09 16:10:21 +0100

    starting scripts

---
+++ Analysis/X_PlotLapseProb.m
@@ -0,0 +1,6 @@
+% retrofit microsleep/burst timecourse scripts to produce output
+% This script compares the effects of:
+% - distance from center (50% split)
+% - eyeclosure
+% - theta burst
+% - alpha bursts
---
+++ Analysis/X_PowervsBursts.m
@@ -0,0 +1,3 @@
+% plot spectrum with and without theta and alpha bursts to show success of
+% algorithm
+% use average of all channels
