b0659f93f3ab1b8783af2b0824d6419badb70e71
Sophia Snipes <code@sophia.science>
2022-12-09 15:04:16 +0100

    copied scripts from 2process-bursts

---
+++ BurstDetection/B_DetectBursts.m
@@ -0,0 +1,114 @@
+% loads in filtered data, finds the bursts in each channel, removes the
+% overlapping ones.
+% for Lapse-Causes
+
+clear
+clc
+close all
+
+Info = burstParameters();
+
+Paths = Info.Paths;
+Bands = Info.Bands;
+BandLabels = fieldnames(Bands);
+
+Task = 'LAT';
+Refresh = false;
+
+% Parameters for bursts
+BT = Info.BurstThresholds;
+Min_Peaks = Info.Min_Peaks;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% get bursts
+
+
+% folder locations
+Source = fullfile(Paths.Preprocessed, 'Clean', 'Waves', Task); % normal data
+Source_Filtered = fullfile(Paths.Preprocessed, 'Clean', 'Waves_Filtered', Task); % extremely filtered data
+Source_Cuts = fullfile(Paths.Preprocessed, 'Cutting', 'Cuts', Task); % timepoints marked as artefacts
+Destination = fullfile(Paths.Data, 'EEG', 'Bursts_AllChannels', Task);
+
+if ~exist(Destination, 'dir')
+    mkdir(Destination)
+end
+
+Content = getContent(Source);
+for Indx_F = 1:numel(Content)
+
+    % load data
+    Filename_Source = Content{Indx_F};
+    Filename_Filtered = replace(Filename_Source, 'Clean.mat', 'Filtered.mat');
+    Filename_Destination = replace(Filename_Source, 'Clean.mat', 'Bursts.mat');
+    Filename_Cuts = replace(Filename_Source, 'Clean.mat', 'Cuts.mat');
+
+    if exist(fullfile(Destination, Filename_Destination), 'file') && ~Refresh
+        disp(['Skipping ', Filename_Destination])
+        continue
+    elseif contains(Filename_Source, 'P00')
+        continue
+    else
+        disp(['Loading ', Filename_Source])
+    end
+
+    M = load(fullfile(Source, Filename_Source), 'EEG');
+    EEG = M.EEG;
+    fs = EEG.srate;
+
+    % get timepoints without noise
+    if exist(fullfile(Source_Cuts, Filename_Cuts), 'file')
+        NoiseEEG = nanNoise(EEG, fullfile(Source_Cuts, Filename_Cuts));
+        Keep_Points = ~isnan(NoiseEEG.data(1, :));
+    else
+        Keep_Points = ones(1, EEG.pnts);
+    end
+
+    % If present, only use time within triggers
+    Labels = {EEG.event.type};
+    Timepoints = [EEG.event.latency];
+    StartEvents = find(strcmp(Labels, 'S192'), 1, 'first');
+    LastEvent = Timepoints(end);
+
+    if ~isempty(StartEvents)
+        Keep_Points(1:round(Timepoints(StartEvents))) = 0;
+    end
+
+    if ~isempty(LastEvent)
+        Keep_Points(round(LastEvent):end) = 0;
+    end
+
+
+    % need to concatenate structures
+    FiltEEG = EEG;
+    FiltEEG.Band = [];
+
+    for Indx_B = 1:numel(BandLabels) % get bursts for all provided bands
+
+        % load in filtered data
+        Band = Bands.(BandLabels{Indx_B});
+        F = load(fullfile(Source_Filtered, BandLabels{Indx_B}, Filename_Filtered));
+        FiltEEG(Indx_B) = F.FiltEEG;
+    end
+
+    % get bursts in all data
+    AllBursts = getAllBursts(EEG, FiltEEG, BT, Min_Peaks, Bands, Keep_Points);
+
+    % keep track of how much data is being used
+    EEG.keep_points = Keep_Points;
+    EEG.clean_t = nnz(Keep_Points);
+
+    EEG.data = []; % only save the extra ICA information
+
+    % save structures
+    parsave(Destination, Filename_Destination, AllBursts, EEG)
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% functions
+
+function parsave(Destination, Filename_Destination, AllBursts, EEG)
+save(fullfile(Destination, Filename_Destination), "AllBursts", "EEG")
+end
+
---
+++ BurstDetection/C_BurstProperties.m
@@ -0,0 +1,80 @@
+% This is to identify additional features of the bursts that isn't just the
+% individual peaks. Also aggregates them across channels.
+% Lapse-causes
+
+clear
+clc
+close all
+
+Info = burstParameters();
+
+Paths = Info.Paths;
+Bands = Info.Bands;
+Tasks = Info.Tasks;
+MinFreqRange = 1; % min difference in frequency from reference burst
+
+Refresh = false;
+
+for Indx_T = 1:numel(Tasks)
+    Task = Tasks{Indx_T};
+
+    Source_Bursts = fullfile(Paths.Data, 'EEG', 'Bursts_AllChannels', Task);
+    Source_EEG = fullfile(Paths.Preprocessed, 'Clean', 'Waves', Task);
+
+    Destination_Bursts = fullfile(Paths.Data, 'EEG', 'Bursts', Task);
+    if ~exist(Destination_Bursts, 'dir')
+        mkdir(Destination_Bursts)
+    end
+
+    Content = getContent(Source_Bursts);
+    Content(~contains(Content, 'Burst')) = [];
+
+    % loop through all files
+    for Indx_F = 1:numel(Content)
+
+        Filename_Bursts = Content{Indx_F};
+        Filename_EEG = replace(Filename_Bursts, 'Bursts.mat', 'Clean.mat');
+
+        if exist(fullfile(Destination_Bursts, Filename_Bursts), 'file') && ~Refresh
+            disp(['Skipping ', Filename_Bursts])
+            continue
+        else
+            disp(['Loading ', Filename_Bursts])
+        end
+
+        load(fullfile(Source_EEG, Filename_EEG), 'EEG')
+
+        fs = EEG.srate;
+        [nCh, nPnts] = size(EEG.data);
+        Data = EEG.data;
+
+        % load bursts
+        load(fullfile(Source_Bursts, Filename_Bursts), 'AllBursts', 'EEG')
+        EEG.data = Data;
+
+        % get frequency of each burst
+        AllBursts = meanFreq(AllBursts);
+
+        % assemble bursts
+        if ~strcmp(Filename_EEG(1:3), 'P00') % skip null file, since it will not have simultaneous bursts
+            Bursts = aggregateBurstsByFrequency(AllBursts, EEG, MinFreqRange);
+        else
+            Bursts = AllBursts;
+        end
+
+        % get properties of the main channel
+        Bursts = burstPeakProperties(Bursts, EEG);
+        Bursts = meanBurstPeakProperties(Bursts); % just does the mean of the main peak's properties
+
+        % classify the burst
+        Bursts = classifyBursts(Bursts);
+
+        % save
+        EEG.data = [];
+        save(fullfile(Destination_Bursts, Filename_Bursts), 'Bursts', 'EEG')
+        disp(['Finished ', Filename_Bursts])
+        clear EEG
+    end
+end
+
+
