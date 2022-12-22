92741b6589295f1de643b8023c55cb699063f8dd
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-12-22 19:39:01 +0100

    moved a little section into a function

--- Analysis/L4_BurstTimecourse.m
+++ Analysis/L4_BurstTimecourse.m
@@ -23,7 +23,7 @@ fs = Parameters.fs;
 
 ConfidenceThreshold = Parameters.EC_ConfidenceThreshold;
 minTrials = Parameters.MinTypes;
-minNanProportion = 0.3; % any more nans than this in a given trial is grounds to exclude the trial
+minNanProportion = 0.5; % any more nans than this in a given trial is grounds to exclude the trial
 
 Pool = fullfile(Paths.Pool, 'EEG'); % place to save matrices so they can be plotted in next script
 BurstPath = fullfile(Paths.Data, 'EEG', 'Bursts', Task);
@@ -38,12 +38,14 @@ Q = quantile(Trials.Radius, 0.5);
 
 t_window = linspace(StartTime, EndTime, fs*(EndTime-StartTime));
 
-ProbBurst = nan(numel(Participants), 3, 2, numel(t_window));
+ProbBurst_Stim = nan(numel(Participants), 3, 2, numel(t_window));
+ProbBurst_Resp = nan(numel(Participants), 3, 2, numel(t_window));
 GenProbBurst = zeros(numel(Participants), numel(BandLabels), 2);
 
 for Indx_P = 1:numel(Participants)
 
-    AllTrials = [];
+    AllTrials_Stim = [];
+    AllTrials_Resp = [];
     AllTrials_Table = table();
 
     for Indx_S = 1:numel(Sessions)
@@ -74,20 +76,38 @@ for Indx_P = 1:numel(Participants)
         BurstTime(:, not(t_valid)) = nan;
 
         % cut put each trial, pool together
-        Trials_B = nan(nTrials, numel(BandLabels), numel(t_window));
+        Trials_B_Stim = nan(nTrials, numel(BandLabels), numel(t_window));
+        Trials_B_Resp = nan(nTrials, numel(BandLabels), numel(t_window));
 
         for Indx_T = 1:nTrials
+
+            % stimulus locked
             StimT = round(fs*Trials.StimTime(CurrentTrials(Indx_T)));
             Start = StimT+StartTime*fs;
             End = StimT+EndTime*fs-1;
 
             Trial = BurstTime(:, Start:End); % just keep track of eyes closed
-            Trials_B(Indx_T, :, :) = Trial;
+            Trials_B_Stim(Indx_T, :, :) = Trial;
+
+
+            % response locked
+            RespT =  round(fs*Trials.RespTime(CurrentTrials(Indx_T)));
+
+            if isnan(RespT)
+                continue
+            end
+
+            Start = RespT+StartTime*fs;
+            End = RespT+EndTime*fs-1;
+
+            Trial = BurstTime(:, Start:End); % just keep track of eyes closed
+            Trials_B_Resp(Indx_T, :, :) = Trial;
         end
 
 
         % pool sessions
-        AllTrials = cat(1, AllTrials, Trials_B);
+        AllTrials_Stim = cat(1, AllTrials_Stim, Trials_B_Stim);
+        AllTrials_Resp = cat(1, AllTrials_Resp, Trials_B_Resp);
 
         % save info
         AllTrials_Table = cat(1, AllTrials_Table, Trials(CurrentTrials, :));
@@ -102,8 +122,6 @@ for Indx_P = 1:numel(Participants)
     end
 
     %%% get probability of microsleep (in time) for each trial type
-
-    Closest =  AllTrials_Table.Radius <= Q;
     nTrials_Nans = 0;
 
     for Indx_B = 1:numel(BandLabels)
@@ -112,20 +130,11 @@ for Indx_P = 1:numel(Participants)
             % choose trials
             Trial_Indexes = AllTrials_Table.Type==Indx_TT; %% & Closest; % & AllTrials_Table.EC==0;
             nTrials = nnz(Trial_Indexes);
-            TypeTrials = squeeze(AllTrials(Trial_Indexes, Indx_B, :));
+            TypeTrials_Stim = squeeze(AllTrials_Stim(Trial_Indexes, Indx_B, :));
 
-            NanProportion = sum(isnan(TypeTrials), 2)/numel(t_window);
+            ProbBurst_Stim(Indx_P, Indx_TT, Indx_B, :)  = ...
+                probEvent(TypeTrials_Stim, minNanProportion, minTrials);
 
-
-            % check if there's enough data
-            Nans = sum(isnan(TypeTrials), 1);
-            if isempty(TypeTrials) || nTrials < minTrials || any(nTrials - Nans < minTrials) % makes sure every timepoint had at least 10 trials
-                continue
-            end
-
-            % average trials
-            nTrialsPoints = sum(TypeTrials==1)+sum(TypeTrials==0); % for each timepoint
-            ProbBurst(Indx_P, Indx_TT, Indx_B, :)  = sum(TypeTrials, 1, 'omitnan')./nTrialsPoints;
         end
     end
 
@@ -135,8 +144,8 @@ end
 
 % remove all data from participants missing any of the trial types
 for Indx_P = 1:numel(Participants)
-    if any(isnan(ProbBurst(Indx_P, :, :)), 'all')
-        ProbBurst(Indx_P, :, :) = nan;
+    if any(isnan(ProbBurst_Stim(Indx_P, :, :)), 'all')
+        ProbBurst_Stim(Indx_P, :, :) = nan;
     end
 end
 
@@ -145,4 +154,4 @@ GenProbBurst = GenProbBurst(:, :, 1)./GenProbBurst(:, :, 2);
 
 %%% save
 t = t_window;
-save(fullfile(Pool, 'ProbBurst.mat'), 'ProbBurst', 't', 'GenProbBurst')
+save(fullfile(Pool, 'ProbBurst.mat'), 'ProbBurst_Stim', 't', 'GenProbBurst')
---
+++ functions/general/probEvent.m
@@ -0,0 +1,29 @@
+function Prob = probEvent(AllTrials, minNanProportion, minTrials)
+% gets the probability of an event.
+% AllTrials is a Trials x time matrix of ones and zeros and nans
+% TrialIndexes is a T x 1 of ones and zeros
+% MinTrials is the minimum number of trials to keep, otherwise Prob is just
+% nans.
+% minNanProportion is the minimum timepoints that can be nans before
+% removing that trial from the batch
+
+Dims = size(AllTrials);
+nTrials = Dims(1);
+Pnts = Dims(2);
+
+
+% make all trial nan if there's not much of it
+NanProportion = sum(isnan(AllTrials), 2)/Pnts;
+AllTrials(NanProportion>minNanProportion, :) = nan;
+
+% check if there's enough data
+Nans = sum(isnan(AllTrials), 1);
+if isempty(AllTrials) || nTrials < minTrials || any(nTrials - Nans < minTrials) % makes sure every timepoint had at least 10 trials
+    Prob = nan(1, Pnts);
+    return
+end
+
+
+% average trials
+nTrialsPoints = sum(AllTrials==1)+sum(AllTrials==0); % for each timepoint
+Prob = sum(AllTrials, 1, 'omitnan')./nTrialsPoints;
