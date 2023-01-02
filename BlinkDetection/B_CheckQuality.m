90fdaccf2c52700aa594d5a8313f3ebea051b84c
Sophia Snipes <code@sophia.science>
2023-01-02 18:47:18 +0100

    little fixes

--- BlinkDetection/B_CheckQuality.m
+++ BlinkDetection/B_CheckQuality.m
@@ -6,6 +6,7 @@ clc
 close all
 
 Info = blinkParameters();
+Paths = Info.Paths;
 Participants = Info.Participants;
 Sessions = Info.Sessions;
 Task = 'LAT';
@@ -14,7 +15,7 @@ Task = 'LAT';
 
 for Indx_P = 1:numel(Participants)
 
-    if mod(Indx_P, 5) == 0
+    if mod(Indx_P, 5) == 0 || Indx_P == 1
         figure('Units', 'normalized', 'OuterPosition', [0 0 1 1])
         Indx = 0;
     end
@@ -22,15 +23,14 @@ for Indx_P = 1:numel(Participants)
         Indx = Indx+1;
 
         Path = fullfile(Paths.Preprocessed, 'Pupils', Task);
-Pupil = loadMATFile(Path, Participants{Indx_P}, Sessions{Indx_S}, 'Pupil');
+        Pupil = loadMATFile(Path, Participants{Indx_P}, Sessions{Indx_S}, 'Pupil');
         if isempty(Pupil); continue; end
 
         subplot(5, numel(Sessions), Indx);
         histogram(Pupil.confidence(Pupil.eye_id==0 & strcmp(Pupil.method, '2d c++')), 'BinEdges',0:.1:1, 'DisplayName', 'eye0')
         hold on
-histogram(Pupil.confidence(Pupil.eye_id==1 & strcmp(Pupil.method, '2d c++')), 'BinEdges',0:.1:1, 'DisplayName', 'eye1')
+        histogram(Pupil.confidence(Pupil.eye_id==1 & strcmp(Pupil.method, '2d c++')), 'BinEdges',0:.1:1, 'DisplayName', 'eye1')
         legend
-title(strjoin({Participants{Indx_P}, Sessions{Indx_S}}, ' '))
-
+        title(strjoin({Participants{Indx_P}, Sessions{Indx_S}}, ' '))
     end
 end
