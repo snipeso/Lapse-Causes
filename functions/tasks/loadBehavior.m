9a2ed036e7553fc9b4c9f391c70955f0be4b1809
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2023-01-05 15:20:45 +0100

    remove extra code from previous thngs

--- functions/tasks/loadBehavior.m
+++ functions/tasks/loadBehavior.m
@@ -1,4 +1,4 @@
-function [Trials, RT, Types, Tots] = loadBehavior(Participants, Sessions, Task, Paths, Refresh)
+function Trials = loadBehavior(Participants, Sessions, Task, Paths, Refresh)
 % single function to load main outcomes for PVT and LAT.
 % LAT types are Lapses, Late, and Correct Responses.
 % PVT types are Lapses (<.5), Correct Responses and Bugs.
@@ -12,9 +12,7 @@ end
 
 Filename_Table = [Task, '_AllAnswers.mat'];
 
-AllData = struct();
-
-% get behavior data
+% get behavior data from RAW data structure if table doesn't already exist
 if ~exist(fullfile(Filepath_Table, Filename_Table), 'file') || Refresh
     AllAnswers = importTask(Paths.Datasets, Task, Filepath_Table);
 else
@@ -34,17 +32,5 @@ end
 % include only data in selected participants & sessions
 Trials = Answers(ismember(Answers.Participant, Participants) & ...
     ismember(Answers.Session, Sessions), :);
-
-
-% RT
-[RT, ~] = tabulateTable(Trials, 'RT', 'mean', Participants, Sessions, []);
-
-
-% lapses, correct responses, etc
-[Types, ~] = tabulateTable(Trials, 'Type', 'tabulate', Participants, Sessions, []);
-
-% total trials
-Tots = squeeze(sum(Types, 3, 'omitnan'));
-
 
 
