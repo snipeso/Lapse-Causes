2b1081061aed3f63c4c3064632bcc4d85fcec82d
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2023-01-06 15:06:00 +0100

    instead of z-score gen prb, subtract from data

--- functions/general/zscoreTimecourse.m
+++ functions/general/zscoreTimecourse.m
@@ -8,34 +8,34 @@ zGenProb = nan(size(GenProb));
 
 if isempty(PreserveDim) % microsleeps
     for Indx_P = 1:Dims(1)
-        Prob = ProbAll(Indx_P, :, :);
+        Prob = ProbAll(Indx_P, :, :)-GenProb(Indx_P); % make it a z-score of difference from general probability
         MEAN = mean(Prob, 'all', 'omitnan');
         STD = std(Prob, 0, 'all', 'omitnan');
         zProb(Indx_P, :, :) = (Prob-MEAN)./STD;
 
-        zGenProb(Indx_P) = (GenProb(Indx_P)-MEAN)./STD;
+        zGenProb(Indx_P) = 0;
     end
 
 elseif PreserveDim==3 % bursts
     for Indx_P = 1:Dims(1)
         for Indx_B = 1:Dims(3)
-            Prob = ProbAll(Indx_P, :, Indx_B, :);
+            Prob = ProbAll(Indx_P, :, Indx_B, :)-GenProb(Indx_P, Indx_B);
             MEAN = mean(Prob, 'all', 'omitnan');
             STD = std(Prob, 0, 'all', 'omitnan');
             zProb(Indx_P, :, Indx_B, :) = (Prob-MEAN)./STD;
 
-            zGenProb(Indx_P, Indx_B) = (GenProb(Indx_P, Indx_B)-MEAN)./STD;
+            zGenProb(Indx_P, Indx_B) = 0;
         end
     end
 elseif PreserveDim==4 % bursts
     for Indx_P = 1:Dims(1)
         for Indx_B = 1:Dims(4)
-            Prob = ProbAll(Indx_P, :, :, Indx_B, :);
+            Prob = ProbAll(Indx_P, :, :, Indx_B, :)-GenProb(Indx_P, :, Indx_B);
             MEAN = mean(Prob, 'all', 'omitnan');
             STD = std(Prob, 0, 'all', 'omitnan');
             zProb(Indx_P, :, :, Indx_B, :) = (Prob-MEAN)./STD;
 
-            zGenProb(Indx_P, :, Indx_B) = (GenProb(Indx_P, :, Indx_B)-MEAN)./STD;
+            zGenProb(Indx_P, :, Indx_B) = 0;
         end
     end
 end
