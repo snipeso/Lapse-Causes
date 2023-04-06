e10ee25fb5699962527d1853a5481593d832ef8c
Sophia Snipes <snipeso@users.noreply.github.com>
2023-04-06 13:41:24 +0200

    deleted and renamed scripts, so only needed ones remain

--- Analysis/QC1_Peaks.m
+++ Analysis/QC1_Peaks.m
@@ -1,9 +1,9 @@
-% script to see how much the bursts occupy EEG (to see if reasonable to
-% assume they also cause the lapses)
+% Control script to get info on bursts occurance in time relative to single
+% peaks (Hung / Bernardi method).
 
 clear
 clc
-close all
+% close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Parameters
@@ -18,23 +18,22 @@ Bands = P.Bands;
 Parameters = P.Parameters;
 
 fs = Parameters.fs;
-ConfidenceThreshold = Parameters.EC_ConfidenceThreshold; % for classifying eyes closed/open
 
 SessionBlocks = P.SessionBlocks;
 SB_Labels = {'BL', 'SD'};
-BandLabels = fieldnames(Bands);
 
 Pool = fullfile(Paths.Pool, 'EEG');
 
-EyePath = fullfile(Paths.Data, ['Pupils_', num2str(fs)], Task);
+PeakPath = fullfile(Paths.Data, 'EEG', 'Peaks', '5_9', Task);
 BurstPath = fullfile(Paths.Data, 'EEG', 'Bursts', Task);
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Load data
 
 Durations = nan(numel(Participants), numel(SB_Labels));
-TimeSpent = nan(numel(Participants), numel(SB_Labels), numel(BandLabels)+1); % duration of bursts individually, and overlapping
+TimeSpent = nan(numel(Participants), numel(SB_Labels), 3); % to compare overlap in time of bursts and peaks
+TotPeaks = nan(numel(Participants), numel(SB_Labels));
+TotBursts =nan(numel(Participants), numel(SB_Labels)); 
 
 for Indx_P = 1:numel(Participants)
     for Indx_SB = 1:numel(SB_Labels) % BL vs SD
@@ -46,60 +45,51 @@ for Indx_P = 1:numel(Participants)
             Bursts = loadMATFile(BurstPath, Participants{Indx_P}, Sessions{Indx_S}, 'Bursts');
             if isempty(Bursts); continue; end
 
+           
+
+            % load peak data
+            Peaks = loadMATFile(PeakPath, Participants{Indx_P}, Sessions{Indx_S}, 'TopPeaks');
+            if isempty(Peaks); continue; end
+
+            TotPeaks(Indx_P, Indx_SB) = numel(Peaks);
+
             % load in EEG data
             EEG = loadMATFile(BurstPath, Participants{Indx_P}, Sessions{Indx_S}, 'EEG');
             Pnts = EEG.pnts;
             ValidTime = EEG.valid_t; % vector of 1s of all the time in which the task was active, and there wasn't noise
-
-            % load eye-data
-            Eyes = loadMATFile(EyePath, Participants{Indx_P}, Sessions{Indx_S}, 'Eyes');
-            if isempty(Eyes); continue; end
-
-            if isnan(Eyes.DQ) || Eyes.DQ == 0
-                EyeOpen = nan(1, Pnts);
-                warning('Bad data eye data')
-                continue
-            end
-
-            Eye = round(Eyes.DQ); % which eye
-            [EyeOpen, ~] = classifyEye(Eyes.Raw(Eye, :), fs, ConfidenceThreshold);
-
-            % look only at eye-open clean data
-            ValidTime = ValidTime & EyeOpen == 1;
 
             % get task duration
             Durations(Indx_P, Indx_SB) = ...
                 Add(Durations(Indx_P, Indx_SB), nnz(ValidTime)/fs);
 
 
-            %%% get time spent with each burst band
-            BurstFreqs = [Bursts.Frequency];
-            AllBurstTime = [];
-            for Indx_B = 1:numel(BandLabels)
-                Band = Bands.(BandLabels{Indx_B});
-                BurstTime = bursts2time(Bursts(BurstFreqs>=Band(1) & BurstFreqs<Band(2)), Pnts);
-                BurstTime = BurstTime & ValidTime; % only consider bursts during task
-                AllBurstTime = cat(1, AllBurstTime, BurstTime);
+            %%% Get amount of overlap in time
+            % Get times with peaks (only negative zero-crossings)
+            PeakTime = peaks2time(Peaks, 'MidDownID', 'MidUpID', Pnts);
+            PeakTime = PeakTime & ValidTime;
 
-                % overall time
-                TimeSpent(Indx_P, Indx_SB, Indx_B) = ...
-                    Add(TimeSpent(Indx_P, Indx_SB, Indx_B), nnz(BurstTime)/fs);
-            end
+            TimeSpent(Indx_P, Indx_SB, 1) = Add(TimeSpent(Indx_P, Indx_SB, 1), nnz(PeakTime)/fs);
+
+            % get time spent with theta bursts
+            BurstFreqs = [Bursts.Frequency];
+%             Band = Bands.Theta;
+Band = [5 9];
+            BurstTime = bursts2time(Bursts(BurstFreqs>=Band(1) & BurstFreqs<Band(2)), Pnts);
+            BurstTime = BurstTime & ValidTime; % only consider bursts during task
+
+             TotBursts(Indx_P, Indx_SB) = numel(Bursts(BurstFreqs>=Band(1) & BurstFreqs<Band(2)));
+            TimeSpent(Indx_P, Indx_SB, 2) = Add(TimeSpent(Indx_P, Indx_SB, 2), nnz(BurstTime)/fs);
 
             % get overlap
-            BothBurstTime = all(AllBurstTime, 1); % when there is both theta and alpha (it gets subtracted later)
+            BothBurstTime = BurstTime & PeakTime; % when there is both theta and alpha (it gets subtracted later)
             TimeSpent(Indx_P, Indx_SB, end) = ...
                 Add(TimeSpent(Indx_P, Indx_SB, end), nnz(BothBurstTime)/fs);
         end
 
         %%% remove overlapping bands times and normalize by total duration
         Duration = Durations(Indx_P, Indx_SB);
-        for Indx_B = 1:numel(BandLabels)
-            TimeSpent(Indx_P, Indx_SB, Indx_B) = (TimeSpent(Indx_P, Indx_SB, Indx_B)-...
-                TimeSpent(Indx_P, Indx_SB, end))/Duration;
-        end
 
-        TimeSpent(Indx_P, Indx_SB, end) = TimeSpent(Indx_P, Indx_SB, end)/Duration;
+        TimeSpent(Indx_P, Indx_SB, :) = TimeSpent(Indx_P, Indx_SB, :)/Duration;
     end
 
     disp(['Finished ', Participants{Indx_P}])
