c489fc675fc3ba2f397b901ee011628e3e4f86b3
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-25 14:50:49 +0200

    move burst detection script

--- Analysis/A_DetectBursts.m
+++ Analysis/A_DetectBursts.m
@@ -1,20 +1,65 @@
+% Detects bursts in EEG data, saves them.
+
 clear
 clc
 close all
 
-Parameters = burstParameters();
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% load in and set parameters for analysis
+
+% set parameters for how you want to run the script this time
+RunParallelBurstDetection = true; % true for faster processing
+RerunAnalysis = false; % false to skip files already analyzed
+Task = 'LAT';
+
+%%% criteria to find bursts in single channels
+% irregular shaped bursts, few criteria, but needs more cycles
+Idx = 1; % this is to make it easier to skip some
+CriteriaSets = struct();
+CriteriaSets(Idx).PeriodConsistency = .6;
+CriteriaSets(Idx).MonotonicityInAmplitude = .6;
+CriteriaSets(Idx).FlankConsistency = .6;
+CriteriaSets(Idx).AmplitudeConsistency = .6;
+CriteriaSets(Idx).MinCyclesPerBurst = 5;
+% % without periodneg, to capture bursts that accelerate/decelerate
+
+% short bursts, strict monotonicity requirements
+Idx = Idx+1;
+CriteriaSets(Idx).PeriodConsistency = .7;
+CriteriaSets(Idx).MonotonicityInAmplitude = .9;
+CriteriaSets(Idx).PeriodNeg = true;
+CriteriaSets(Idx).FlankConsistency = 0.3;
+CriteriaSets(Idx).MinCyclesPerBurst = 3;
+
+% relies on shape but low other criteria; gets most of the bursts
+Idx = Idx+1;
+CriteriaSets(Idx).PeriodConsistency = .5;
+CriteriaSets(Idx).MonotonicityInTime = .4;
+CriteriaSets(Idx).MonotonicityInAmplitude = .4;
+CriteriaSets(Idx).ReversalRatio = 0.6;
+CriteriaSets(Idx).ShapeConsistency = .2;
+CriteriaSets(Idx).FlankConsistency = .5;
+CriteriaSets(Idx).MinCyclesPerBurst = 3;
+CriteriaSets(Idx).AmplitudeConsistency = .4;
+CriteriaSets(Idx).MinCyclesPerBurst = 4;
+CriteriaSets(Idx).PeriodNeg = true;
+
+MinClusteringFrequencyRange = 1; % to cluster bursts across channels
+
+
+% load in parameters that are in common across scripts
+Parameters = analysisParameters();
 Paths = Parameters.Paths;
 Sessions = Parameters.Sessions;
 Participants = Parameters.Participants;
 Bands = Parameters.Bands;
-CriteriaSets = Parameters.CriteriaSets;
 Triggers = Parameters.Triggers;
 
-RunParallelBurstDetection = false;
-RerunAnalysis = false;
-Task = 'LAT';
-MinClusteringFrequencyRange = 1; % to cluster bursts across channels
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Analysis
+
+% set paths and files
 EEGSource = fullfile(Paths.Preprocessed, 'Clean', 'Waves', Task);
 EEGSourceCuts = fullfile(Paths.Preprocessed, 'Cutting', 'Cuts', Task); % timepoints marked as artefacts
 Destination = fullfile(Paths.Data, 'EEG', 'Bursts_New', Task);
@@ -25,6 +70,9 @@ end
 Filenames = getContent(EEGSource);
 Filenames(~contains(Filenames, Sessions)) = [];
 Filenames(~contains(Filenames, Participants)) = [];
+
+
+%%% run
 
 for FilenameSource = Filenames'
 
@@ -50,14 +98,15 @@ for FilenameSource = Filenames'
         CleanTimepoints = ones(1, EEG.pnts);
     end
 
-    % get vector of points from which the burst data was pooled (task,
-    % clean)
+    % get timepoints of the task
     TaskPoints = zeros(1, numel(CleanTimepoints));
     TriggerTypes = {EEG.event.type};
     TriggerLatencies = [EEG.event.latency];
     StartTask = round(TriggerLatencies(strcmp(TriggerTypes, Triggers.Start)));
     EndTask = round(TriggerLatencies(strcmp(TriggerTypes, Triggers.End)));
     TaskPoints(StartTask:EndTask) = 1;
+
+    % only use clean task timepoints
     KeepTimepoints = CleanTimepoints & TaskPoints;
 
     % filter data into narrowbands
@@ -70,7 +119,7 @@ for FilenameSource = Filenames'
     % aggregate bursts into clusters across channels
     BurstClusters = cycy.aggregate_bursts_into_clusters(Bursts, EEG, MinClusteringFrequencyRange);
 
-        % keep track of how much data is being used
+    % keep track of how much data is being used
     EEG.CleanTaskTimepoints = KeepTimepoints;
     EEG.CleanTaskTimepointsCount = nnz(KeepTimepoints);
     EEG.data = []; % only save the metadata
