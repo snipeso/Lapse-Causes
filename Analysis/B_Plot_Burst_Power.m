eb753600bbce0efa32e70c6e6cc3757fcda52f31
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-28 12:25:59 +0200

    start script

---
+++ Analysis/B_Plot_Burst_Power.m
@@ -0,0 +1,5 @@
+% Evaluates the quality of the burst detection, by plotting the change in
+% spectrum of the data with and without bursts, and providing ratios
+% quantifying how much power was captured by the bursts.
+
+
