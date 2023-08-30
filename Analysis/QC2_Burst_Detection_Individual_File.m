5e95bfda130aa710ce668c1bb4f1a6f763e6c9f5
Sophia Snipes <snipeso@users.noreply.github.com>
2023-08-30 12:10:22 +0200

    Create QC2_Burst_Detection_Individual_File.m

---
+++ Analysis/QC2_Burst_Detection_Individual_File.m
@@ -0,0 +1,29 @@
+% check the burst detection for a specific file
+
+clear
+clc
+close all
+
+Info = analysisParameters();
+Paths = Info.Paths;
+
+
+%%%% Choose a file
+%%%%
+%%%%
+Task = 'LAT'; % Game, Standing, Fixation
+Session = 'BaselineBeam';
+Participant = 'P11'; % P03 has almost no oscillations, P15 has tons
+%%%%
+%%%%
+%%%%
+
+Source_EEG = fullfile(Paths.Data, 'Clean', 'Waves', Task); % normal data
+Source_Bursts = fullfile(Paths.AnalyzedData, 'EEG', 'Bursts_New', Task);
+
+EEG = load_datafile(Source_EEG, Participant, Session, 'EEG');
+SampleRate = EEG.srate;
+
+BurstClusters = load_datafile(Source_Bursts, Participant, Session, 'BurstClusters');
+
+cycy.plot.plot_all_bursts(EEG, 10, BurstClusters, 'Band');
