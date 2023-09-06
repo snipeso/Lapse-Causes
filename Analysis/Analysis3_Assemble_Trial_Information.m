3e7515426ced8e20e2cb6ae552acf6217f3253bf
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-06 16:58:08 +0200

    now save trigger times into trials table

--- Analysis/Analysis3_Assemble_Trial_Information.m
+++ Analysis/Analysis3_Assemble_Trial_Information.m
@@ -40,12 +40,13 @@ for Task = Tasks
     % load output from raw data (or intermediate cache)
     TrialsTable = load_task_output(Participants, Sessions.(Task{1}), Task{1}, Paths, false);
 
+    % determine stimulus trigger times
+    EyetrackingDir = fullfile(Paths.Data, 'Pupils', ['Raw_', num2str(SampleRate), 'Hz'], Task{1});
+    TrialsTable = trigger_times(TrialsTable, EyetrackingDir, Triggers);
 
     %%% determine whether eyes were open or closed
     EyetrackingQualityTable = readtable(fullfile(Paths.QualityCheck, 'EyeTracking', ...
         ['DataQuality_', Task{1}, '_Pupils.csv']));
-
-    EyetrackingDir = fullfile(Paths.Data, 'Pupils', ['Raw_', num2str(SampleRate), 'Hz'], Task{1});
 
     TrialsTable = eyes_closed_trials(TrialsTable, EyetrackingQualityTable, EyetrackingDir, ...
         TrialWindow, MinEventProportion,  MaxNanProportion, ConfidenceThreshold, Triggers);
@@ -73,6 +74,58 @@ end
 % set up cache
 if ~exist(CacheDir, 'dir')
     mkdir(CacheDir)
+end
+end
+
+
+function TrialsTable = trigger_times(TrialsTable, MetadataDir, Triggers)
+
+Participants = unique(TrialsTable.Participant);
+Sessions = unique(TrialsTable.Session);
+
+TrialsTable.StimTimepoint = nan(size(TrialsTable, 1), 1);
+TrialsTable.RespTimepoint = nan(size(TrialsTable, 1), 1);
+
+
+for Participant = Participants'
+    for Session = Sessions'
+
+        % trial info for current recording
+        CurrentTrials = find(strcmp(TrialsTable.Participant, Participant{1}) & ...
+            strcmp(TrialsTable.Session, Session{1}));
+        nTrials = nnz(CurrentTrials);
+        if isempty(nTrials) || nTrials < 5
+            warning(['Missing ', Participant{1}, Session{1}])
+            continue
+        end
+        EEGMetadata = load_datafile(MetadataDir, Participant{1}, Session{1}, 'EEGMetadata');
+        if isempty(EEGMetadata);continue;end
+
+        Events = EEGMetadata.event;
+        Latencies = [Events.latency];
+        Types = {Events.type};
+
+        % gather stimulus latencies
+        StimLatencyIndexes = find(strcmp(Types, Triggers.Stim));
+        StimLatencies = Latencies(StimLatencyIndexes);
+
+        if numel(StimLatencies) ~= nTrials
+            error('mismatch trials and triggers stimuli')
+        end
+        TrialsTable.StimTimepoint(CurrentTrials) = StimLatencies;
+
+        % gather response latencies
+
+        RespLatencyIndexes = StimLatencyIndexes+1; % only responses immediately after a stimulus
+        NextTriggerType = Types(RespLatencyIndexes);
+        NotResponses = ~strcmp(NextTriggerType, Triggers.Resp);
+        RespLatencyIndexes(NotResponses) = [];
+        RespLatencies = Latencies(RespLatencyIndexes);
+        ResponseCurrentTrials = CurrentTrials; % skip lapses
+        ResponseCurrentTrials(NotResponses) = [];
+
+        TrialsTable.RespTimepoint(ResponseCurrentTrials) = RespLatencies;
+    end
 end
 end
 
--- functions/general/chop_trials.m
+++ functions/general/chop_trials.m
@@ -1,14 +1,7 @@
-function TrialData = chop_trials(Data, SampleRate, Events, Trigger, Window)
+function TrialData = chop_trials(Data, SampleRate, TriggerLatencies, Window)
 
-Latencies = [Events.latency];
-Types = {Events.type};
-ChannelCount = size(Data, 1);
-
-StimLatencyIndexes = strcmp(Types, Trigger);
-StimLatencies = Latencies(StimLatencyIndexes);
-
-Starts = round(StimLatencies-Window(1)*SampleRate);
-Ends = round(StimLatencies+Window(2)*SampleRate);
+Starts = round(TriggerLatencies-Window(1)*SampleRate);
+Ends = round(TriggerLatencies+Window(2)*SampleRate);
 
 TrialData = nan(numel(Starts),ChannelCount, Ends(1)-Starts(1)+1);
 for idxTrials = 1:numel(Starts)
