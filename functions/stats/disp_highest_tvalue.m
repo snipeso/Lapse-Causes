bdd30d407f230b20b1902a399dc9ffe5707c02d4
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-12 11:07:47 +0200

    rename dispMaxTChanlocs

---
+++ functions/stats/disp_highest_tvalue.m
@@ -0,0 +1,13 @@
+function disp_highest_tvalue(Stats, Chanlocs, String)
+
+% if ~any(Stats.sig)
+%     return
+% end
+
+t = Stats.t;
+% t(~Stats.sig) = nan;
+Stats.p_fdr = Stats.p_fdr(:);
+Stats.N = repmat(Stats.N, numel(Stats.p_fdr), 1);
+[~, Indx] = max(abs(t));
+
+disp_stats(Stats, [Indx, 1], [String, ' ', Chanlocs(Indx).labels]);
