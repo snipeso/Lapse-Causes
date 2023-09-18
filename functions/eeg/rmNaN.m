db23ed96cd13317d73bc2cb2815829b24d159066
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-18 15:27:08 +0200

    remove unneeded functions

--- functions/eeg/rmNaN.m
+++
@@ -1,8 +0,0 @@
-function EEG = rmNaN(EEG)
-% properly removes all data marked by NaNs
-
-isNaN = isnan(mean(EEG.data, 1));
-
-[Starts, Ends] = data2windows(isNaN); 
-
-EEG = pop_select(EEG, 'nopoint', [Starts(:), Ends(:)]);
