5a209fd426cdb8382db99f7380d83f681c23ae9e
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-18 15:16:30 +0200

    fix check eyetracking quality

--- Analysis/QC2_Eyetracker_Quality.m
+++ Analysis/QC2_Eyetracker_Quality.m
@@ -1,16 +1,20 @@
 
 % plots the distribution of confidence values for each eye and each
-% participant so I can chose which eye to use for each recording. 
+% participant so I can chose which eye to use for each recording. Ideally,
+% each histogram should show mostly 1s, and some 0s. The more uniform the
+% distrinution, the worst the eye quality. In a csv, save which eye had the
+% best distribution. 
+
 clear
 clc
 close all
 
-Info = blinkParameters();
-Paths = Info.Paths;
-Participants = Info.Participants;
-Sessions = Info.Sessions;
+Parameters = analysisParameters();
+Paths = Parameters.Paths;
+Participants = Parameters.Participants;
 
 Task = 'PVT';
+Sessions = Parameters.Sessions.(Task);
 
 
 for Indx_P = 1:numel(Participants)
@@ -22,14 +26,16 @@ for Indx_P = 1:numel(Participants)
     for Indx_S = 1:numel(Sessions)
         Indx = Indx+1;
 
-        Path = fullfile(Paths.Data, 'Pupils', Task);
+        Path = fullfile(Paths.Data, 'Pupils', 'Raw', Task);
         Pupil = load_datafile(Path, Participants{Indx_P}, Sessions{Indx_S}, 'Pupil');
         if isempty(Pupil); continue; end
 
         subplot(5, numel(Sessions), Indx);
-        histogram(Pupil.confidence(Pupil.eye_id==0 & strcmp(Pupil.method, '2d c++')), 'BinEdges',0:.1:1, 'DisplayName', 'eye0')
+        histogram(Pupil.confidence(Pupil.eye_id==0 & strcmp(Pupil.method, '2d c++')), ...
+            'BinEdges',0:.1:1, 'DisplayName', 'eye0')
         hold on
-        histogram(Pupil.confidence(Pupil.eye_id==1 & strcmp(Pupil.method, '2d c++')), 'BinEdges',0:.1:1, 'DisplayName', 'eye1')
+        histogram(Pupil.confidence(Pupil.eye_id==1 & strcmp(Pupil.method, '2d c++')), ...
+            'BinEdges',0:.1:1, 'DisplayName', 'eye1')
         legend
         title(strjoin({Participants{Indx_P}, Sessions{Indx_S}}, ' '))
     end
