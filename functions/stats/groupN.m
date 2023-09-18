2ac4fe2e6ed432942bb14f4b68e7d4c094954acd
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-18 15:46:41 +0200

    remove old scripts

--- functions/stats/groupN.m
+++
@@ -1,8 +0,0 @@
-function groupN(CohenD)
-
-u = 1.96; % for alpha = 5%
-v = 1.28; % for beta = 10%
-
-N = round((2*(u+v)^2)/(CohenD)^2);
-
-disp(['For Cohen D of ', num2str(CohenD), ', N = ', num2str(N)])
