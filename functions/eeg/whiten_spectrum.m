9f4d5b32c4dd43eb28c251e2ba18b6ef8c48c835
Sophia Snipes <snipeso@users.noreply.github.com>
2023-09-19 22:00:05 +0200

    cleanup

--- functions/eeg/whiten_spectrum.m
+++ functions/eeg/whiten_spectrum.m
@@ -5,4 +5,3 @@ FooofModel = fooof(Frequencies, Power, FooofFittingRange, struct(), true);
 FooofFrequencies = FooofModel.freqs;
 
 WhitenedPower = 10.^FooofModel.power_spectrum-10.^FooofModel.ap_fit;
-% WhitenedPower = FooofModel.power_spectrum-FooofModel.ap_fit;
